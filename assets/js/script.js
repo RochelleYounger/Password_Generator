a635e96b2d9470ede257909cbcfdeeeeee9e54f5
Rochelle Younger <regnuoy321@gmail.com>
2021-12-14 15:28:36 -0500

    add password generation function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,15 @@ var checkboxSpecial = document.getElementById("char-special");
 var passwordLength = document.getElementById("password-length");
 
 // character arrays
-var charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", 
-"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-var charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "U", "J", "K", "L", "M", "N", "O", 
-"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-var charNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var charSpecial = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", 
-"{", "}", "[", "]", "|", "<", ">", "/", "?", "`", "'",, ",", ".", ";", ":", "\\"];
-var useableChar = [];
+var charLower = "abcdefghijklmnopqrstuvwxyz";
+var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charNum = "012345679";
+var charSpecial = "~`!@#$%^&*()_-+=[]{}\\|,./<>?";
+var useableChar = "";
 
 var lowerCheck = function () {
   if (checkboxLower.checked) {
-    useableChar.push(charLower);
+    useableChar += charLower;
     // console.log("1");
   }else {
     return false;
@@ -26,7 +23,7 @@ var lowerCheck = function () {
 }
 var upperCheck = function () {
   if (checkboxUpper.checked) {
-    useableChar.push(charUpper);
+    useableChar += charUpper;
     // console.log("2");
   }else {
     return false;
@@ -34,7 +31,7 @@ var upperCheck = function () {
 }
 var numCheck = function () {
   if (checkboxNum.checked) {
-    useableChar.push(charNum);
+    useableChar += charNum;
     // console.log("3");
   }else {
     return false;
@@ -42,14 +39,14 @@ var numCheck = function () {
 }
 var specialCheck = function () {
   if (checkboxSpecial.checked) {
-    useableChar.push(charSpecial);
+    useableChar += charSpecial;
     // console.log("4");
   }else {
     return false;
   }
 }
 //updating password character array with user selection
-var generateArray = function () {
+var generateCharSet = function () {
   lowerCheck();
   upperCheck();
   numCheck();
@@ -57,16 +54,25 @@ var generateArray = function () {
   // console.log(useableChar);
 };
 
-var passwordString = "";
-var generatePassword = function () {
-  for (var i = 0; i < passwordLength.length; i++) {
-    var arrRef = Math.floor(Math.random() * (useableChar.length));
-    console.log(passwordString);
-    passwordString += useableChar[arrRef];
+// var generatePassword = function () {
+//   var passwordString = "";
+//   for (var i = 0; i < passwordLength.length; i++) {
+//     passwordString = useableChar.indexOf(Math.floor(Math.random() * useableChar.length));
+//     console.log(passwordString);
+//     // passwordString += useableChar[arrRef];
 
+//   }
+// };
+
+function generatePassword() {
+  generateCharSet();
+  console.log(useableChar);
+  var passwordString = "";
+  for (var i = 0, n =useableChar.length; i < passwordLength.value; ++i) {
+      passwordString += useableChar.charAt(Math.floor(Math.random() * n));
   }
-};
-
+  return passwordString;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -76,7 +82,7 @@ function writePassword() {
   // console.log(checkboxUpper.checked);
   // reset useable char [] after password is generated
   console.log(passwordLength.value)
-  // useableChar = [];
+  useableChar = "";
 }
 
 // Add event listener to generate button
